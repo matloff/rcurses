f5bd95d3621d8b391ef291e0d46308acf8a3f226
Norm Matloff <matloff@cs.ucdavis.edu>
2017-12-26 10:41:11 -0800

    added upon-loading message

---
+++ R/onAttach.R
@@ -0,0 +1,6 @@
+
+.onAttach <- function(libname, pkgname) {
+   packageStartupMessage(
+      '\n\n\n\n\n*********************\n\n\n\nLatest version at GitHub.com/matloff\n\n\nType "?rcurses" for function list.')
+}
+
